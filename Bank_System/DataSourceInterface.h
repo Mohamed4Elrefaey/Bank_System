93b478af6a8a703c838346492981a15738ef0297
mohamed <mohammedhamedelrefaey@gmail.com>
2025-09-06 17:13:29 +0300

    First class in phase 2

---
+++ Bank_System/DataSourceInterface.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Client.h"
+#include "Employee.h"
+#include "admin.h"
+
+class DataSourceInterface: public Client , public Employee
+{
+
+	virtual void addClient(Client obj) = 0;
+	virtual void addEmployee(Employee obj) = 0;
+	virtual void addAdmin(Admin obj) = 0 ;
+	virtual void getAllClients() = 0 ;
+	virtual void getAllEmployees() = 0;
+	virtual void getAllAdmins() = 0;
+	virtual void removeAllClients() = 0;
+	virtual void removeAllEmployees() = 0;
+	virtual void removeAllAdmins() = 0;
+
+};
+
